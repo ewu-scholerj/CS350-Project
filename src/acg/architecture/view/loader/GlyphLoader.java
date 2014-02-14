5098328450ce5eba24be73f9d8c448bb232da717
ewu-nguyena <tien.nguyen@eagles.ewu.edu>
2014-02-14 11:46:17 -0800

    Started constructor. Try catch block done for reading in a filename.

--- src/acg/architecture/view/loader/GlyphLoader.java
+++ src/acg/architecture/view/loader/GlyphLoader.java
@@ -1,6 +1,24 @@
 package acg.architecture.view.loader;
 
+import java.util.Scanner;
+
 public class GlyphLoader {
-	System.out.println("Hello World");
+	
+	public GlyphLoader(String filename)
+	{
+		Scanner file;
+		
+		try
+		{
+			file = new Scanner ( new FileReader(filename));
+			
+			file.close();
+		}
+		catch (FileNotFoundException e)
+		{
+			System.out.println("Error with file: Please try again");
+			System.exit(1);
+		}
+	}
 
 }
