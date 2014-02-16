2b51e7443c5b8d1b6c1bb95556b266099108cfa0
ewu-nguyena <tien.nguyen@eagles.ewu.edu>
2014-02-15 20:18:23 -0800

    Added a fill_list method.  Worked on splitting a string up using , and ; and delimiters into a string array called tokens.

--- src/acg/architecture/view/loader/GlyphLoader.java
+++ src/acg/architecture/view/loader/GlyphLoader.java
@@ -1,16 +1,26 @@
 package acg.architecture.view.loader;
+/**
+ * GlyphLoader
+ * 
+ * @author Tien Nguyen
+ */
 
+/**
+ * Uses four stacks: Color, Vertex, Circle, Edge
+ */
 import java.util.Scanner;
 
 public class GlyphLoader {
 	
+	// constructor
 	public GlyphLoader(String filename)
 	{
 		Scanner file;
 		
-		try
+		try		// try catch block for valid file name
 		{
 			file = new Scanner ( new FileReader(filename));
+			fill_lists(file);
 			
 			file.close();
 		}
@@ -18,6 +28,22 @@ public class GlyphLoader {
 		{
 			System.out.println("Error with file: Please try again");
 			System.exit(1);
+		}
+	}
+	
+	// fills the lists
+	private fill_lists(Scanner file)
+	{
+		String temp;
+		String delims = "[,;]+";	// split string on , and ;
+		String[] tokens;
+		
+		while(file.hasNext())
+		{
+			temp = file.next();
+			
+			tokens = temp.split(delims);
+			
 		}
 	}
 
